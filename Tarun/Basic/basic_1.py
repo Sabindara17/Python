7f49d8aa43214631a3145c9a1a64a8d0137972e5
tarunshrestha <tarunshrestha90@gmail.com>
2024-08-04 18:56:28 +0545

    aa

---
+++ Tarun/Basic/basic_1.py
@@ -0,0 +1,31 @@
+my_string = "Ram"
+my_int = 12
+my_float = 15.12
+my_bool = True
+
+print(f"{my_int} as")
+print(f" {my_string} {my_bool} ")
+print(my_string, my_bool)
+
+print("Hello my name is", my_string, ".Active:", my_bool)
+print(f"Hello my name is {my_string}. Active: {my_bool}")
+
+
+# name = input("What is your name?")
+# age  = int(input("What is your age?"))
+
+# print(name)
+# print(age)
+print(round(52.2656565656565656565, 2))
+print(25//4)
+
+
+
+
+"""
+kdasdlkalskjdasd
+asdasd
+asdasd
+asd
+"""
+
