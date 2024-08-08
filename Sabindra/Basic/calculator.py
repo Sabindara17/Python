3f4b1ff76c99cc8414cf85081b484c3811a90481
Sabindara17 <sthasabin17@gmail.com>
2024-08-08 17:37:16 +0545

    basic

--- Sabindra/Basic/calculator.py
+++ Sabindra/Basic/calculator.py
@@ -1,18 +1,23 @@
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-operator = input("Enter the operator(+, -, *, /): ")
-
 def calculator(a, b, operator):
-    if operator == "+":
-        return a + b
-    elif operator == "-":
-        return a - b
-    elif operator == "*":
-        return a * b
-    elif operator == "/":
-        return a / b
-    else:
-        return "error"
+        if operator == "+":
+            return a + b
+        elif operator == "-":
+            return a - b
+        elif operator == "*":
+            return a * b
+        elif operator == "/":
+            return a / b
+        else:
+            return "error"
     
-result = calculator(a, b, operator)
-print(round(result,2)) 
+button = True
+while button:
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    operator = input("Enter the operator(+, -, *, /): ")  
+    result = calculator(a, b, operator)
+    print(round(result,2)) 
+    ans = input("Do you want to turn off? ")
+    if ans.lower() == "yes":
+         button = False
+
